6e462cf9b4faeb5ea569fe32f19d735cd6f389e4
Roh Woohyeon <roh.woohyeon@gmail.com>
2020-10-31 13:43:03 +0900

    Apply prettier

--- src/index.ts
+++ src/index.ts
@@ -30,33 +30,36 @@ export function methodRouter(
     .map(v => v.toUpperCase())
     .join(', ')
 
-  return prismy([contextSelector, methodSelector], (
-    context,
-    /* istanbul ignore next */
-    method = ''
-  ) => {
-    const requestListener = map[method.toLowerCase()]
+  return prismy(
+    [contextSelector, methodSelector],
+    (
+      context,
+      /* istanbul ignore next */
+      method = ''
+    ) => {
+      const requestListener = map[method.toLowerCase()]
 
-    if (requestListener == null) {
-      if (options.errorHandler != null) {
-        return options.errorHandler(method, context)
-      }
-      if (method === 'OPTIONS') {
-        return res(null, 200, {
+      if (requestListener == null) {
+        if (options.errorHandler != null) {
+          return options.errorHandler(method, context)
+        }
+        if (method === 'OPTIONS') {
+          return res(null, 200, {
+            'Access-Control-Request-Method': allowedMethods
+          })
+        }
+        return res('Method Not Allowed', 405, {
           'Access-Control-Request-Method': allowedMethods
         })
       }
-      return res('Method Not Allowed', 405, {
-        'Access-Control-Request-Method': allowedMethods
-      })
+
+      const next = async () => requestListener.contextHandler(context)
+
+      const pipe = middlewareList.reduce((next, middleware) => {
+        return () => middleware(context)(next)
+      }, next)
+
+      return pipe()
     }
-
-    const next = async () => requestListener.contextHandler(context)
-
-    const pipe = middlewareList.reduce((next, middleware) => {
-      return () => middleware(context)(next)
-    }, next)
-
-    return pipe()
-  })
+  )
 }
